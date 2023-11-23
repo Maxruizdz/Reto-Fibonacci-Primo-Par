a799932a0ce069ae3a593d711d2b2706152c88d9
Maxruizdz <maxruizdz@hotmail.com>
2023-11-23 17:15:14 -0300

    Contando palabras de cadena de caracteres

--- InvertirCadena/Program.cs
+++ InvertirCadena/Program.cs
@@ -25,3 +25,37 @@ palabra_invertida_met2= new string(chars);
 Console.WriteLine("Palabra Invertida, Metodo 2: " + "\n" + palabra_invertida_met2);
 
 
+//Contar Palabras 
+int contador_palabra = 0;
+
+Console.WriteLine("Ingrese el caracter que desea saber cuantas veces se repite");
+char a =char.Parse(Console.ReadLine());
+
+
+
+
+foreach (var pala in palabra) {
+
+
+    if (pala == a) {
+        contador_palabra++;
+    
+    
+    }
+
+
+
+}
+
+
+
+Console.WriteLine($"Las veces que se repite la letra {a} es : {contador_palabra} ");
+
+//METODO 2
+int contador_2= palabra.Where(x => x == a).Count();
+
+
+Console.WriteLine($"Las veces que se repite la letra {a} es : {contador_2} ");
+
+
+
