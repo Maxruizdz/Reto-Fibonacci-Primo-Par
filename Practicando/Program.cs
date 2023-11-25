ff4da124901d295520d1fd0e28e344d6646602f1
Maxruizdz <maxruizdz@hotmail.com>
2023-11-25 17:49:14 -0300

    terminacion-Encontrar el numero mas alto de una lista

---
+++ Practicando/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+List<int> list = new List<int>();
+list.Add(130);
+list.Add(4);
+list.Add(20);
+list.Add(14);
+list.Add(12);
+list.Add(100);
+int numero_mayor = 0;
+
+
+foreach (int i in list) {
+
+
+    if (i > numero_mayor)
+    {
+        numero_mayor = i;
+
+    }
+
+
+}
+
+Console.WriteLine($"El numero mayor de la lista es {numero_mayor}");
